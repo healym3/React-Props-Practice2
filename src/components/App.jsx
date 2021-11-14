b59273dcfbf718c0d02f21f16358f035f554d6e5
MacKenzie Healy <79485702+healym3@users.noreply.github.com>
2021-11-14 11:25:06 -0600

    Map data to component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,28 +2,23 @@ import React from "react";
 import Card from "./Card";
 import Contacts from "../contacts";
 
+function createCard(contact) {
+  return (
+    <Card
+      key={contact.id}
+      name={contact.name}
+      imgURL={contact.imgURL}
+      tel={contact.phone}
+      email={contact.email}
+    />
+  );
+}
+
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <Card
-        name={Contacts[0].name}
-        imgURL={Contacts[0].imgURL}
-        tel={Contacts[0].phone}
-        email={Contacts[0].email}
-      />
-      <Card
-        name={Contacts[1].name}
-        imgURL={Contacts[1].imgURL}
-        tel={Contacts[1].phone}
-        email={Contacts[1].email}
-      />
-      <Card
-        name={Contacts[2].name}
-        imgURL={Contacts[2].imgURL}
-        tel={Contacts[2].phone}
-        email={Contacts[2].email}
-      />
+      {Contacts.map(createCard)}
     </div>
   );
 }
